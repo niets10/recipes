4028d607c52578f5ad304e60c6a698f0780e4b0d
agnieto <agnieto00@gmail.com>
2026-02-14 15:00:32 +0100

    Change route

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
 import { Spinner } from '@/components/ui/spinner';
+import { routes } from '@/lib/routes';
 
 export function LogoutButton() {
     const router = useRouter();
@@ -13,7 +14,7 @@ export function LogoutButton() {
     const logout = async () => {
         const supabase = createClient();
         await supabase.auth.signOut();
-        router.push('/auth/login');
+        router.push(routes.login);
     };
 
     const [isPending, startTransition] = useTransition();
